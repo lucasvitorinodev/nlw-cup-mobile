1d3eafe366d6c0e52540cf4062f977731d54a205
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-11-03 10:01:14 -0300

    feat(*): first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { THEME } from "./src/styles/theme"
+import { SignIn } from "./src/screens/SignIn";
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold})
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      { fontsLoaded ? <SignIn/> : <Loading/> }
+    </NativeBaseProvider>
+  );
+}
+
