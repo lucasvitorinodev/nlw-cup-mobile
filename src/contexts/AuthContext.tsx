77c1aec1f6f6c1a9fff5f54827a9708d7516230a
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-11-04 23:15:07 -0300

    feat(auth): auth integrated with google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,18 +23,44 @@ interface AuthProviderProps {
 }
 
 export function AuthContextProvider({ children }) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "792381862425-ltqrh7a67jg0cbnmvbrm0sjhqjjt9g35.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
+
   const signIn = async () => {
-    console.log("Signing in...");
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    } finally {
+      setIsUserLoading(false);
+    }
+  };
+
+  const signInWithGoogle = async (accessToken) => {
+    console.log(accessToken);
   };
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "Lucas",
-          avatarUrl: "https://github.com/lucasvitorinodev.png",
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
