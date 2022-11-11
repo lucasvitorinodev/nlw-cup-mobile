60936cf05422005ad3aeb7a7ed4a24a8df573544
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-11-11 18:50:17 -0300

    feat(*): inline variable

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,7 +2,5 @@ import { useContext } from "react";
 import { AuthContext, AuthContextDataProps } from "../contexts/AuthContext";
 
 export function useAuth(): AuthContextDataProps {
-  const context = useContext(AuthContext);
-
-  return context;
+  return useContext(AuthContext);
 }
